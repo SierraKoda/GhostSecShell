69ad7159f981535c5434c291cf24cbe91f3b9218
OmniPuppy <39969061+SierraKoda@users.noreply.github.com>
2020-05-14 21:19:01 -0600

    Updates Rolling
    index.js && data.json updated for table inclusion of API key objects and root app inclusive runtime build

---
+++ index.js
@@ -0,0 +1,35 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import './index.html';
+import './OmnipuppyXX-Outside-HMAC3SHA512-1.js';
+import './OmnipuppyXX-Outisde-HMAC3SHA512-12.js';
+import './omnipuppyxx-node-js-key-base.js';
+import './lib/blockchain.js';
+import './bin/blockchain.js';
+import './init-server.js';
+import './on-error-server.js';
+import './ssr-module-cache.js';
+import './App.test.js';
+import './_app.js';
+import './_document.js';
+import './_error.js';
+import './App.js';
+import './NewHash.js';
+import './NewHashItem.js';
+import './Error.js';
+import './data.json';
+import './hash.json';
+import './hashSetGroup.json';
+import './hashAttributes.json';
+import './hashItem.json';
+import './blockpair.json';
+import './hashHMACSH512.json';
+const ajax = require('ajax');
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
+serviceWorker.register();
